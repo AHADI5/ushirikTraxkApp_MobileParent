5831183f6fec4cf6fe383584c3c8a9ae14774181
AHADI5 <gloireahadi9@gmail.com>
2024-03-24 08:28:42 +0100

    changing the password icon to a box with a toggle eye

--- lib/pages/parent_login_form.dart
+++ lib/pages/parent_login_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:show_hide_password/show_hide_password.dart';
 
 class ParentLoginPage extends StatefulWidget {
   const ParentLoginPage({super.key});
@@ -27,39 +29,69 @@ class LoginPageState extends State<ParentLoginPage> {
             child: TextFormField(
               //The validator receives the text entred by the user
               keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(),
-                hintText: "Email",
-                prefixIcon: Icon(Icons.email),
-              ),
+              decoration: InputDecoration(
+                  isDense: true,
+                  prefixIcon: const Icon(Icons.email),
+                  hintText: "Email",
+                  hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Colors.black38, fontWeight: FontWeight.w500),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black12, width: 1),
+                      borderRadius: BorderRadius.circular(12)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black38, width: 1),
+                      borderRadius: BorderRadius.circular(12)),
+                  counterText: ""),
               onChanged: (String value) {},
               validator: (value) {
                 if (value == null || value.isEmpty) {
-                  return 'veuiller entrer votre nom';
+                  return 'veuiller entrer votre Email';
                 }
                 return null; // for valid input
               },
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: TextFormField(
-              //The validator receives the text entred by the user
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(),
-                hintText: "Mot de passe",
-                prefixIcon: Icon(Icons.password_sharp),
-              ),
-              onChanged: (String value) {},
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'veuiller entrer votre mot de passe';
-                }
-                return null; // for valid input
-              },
-            ),
-          ),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: ShowHidePassword(passwordField: ((hidePassword) {
+                return TextFormField(
+                  keyboardType: TextInputType.text,
+                  obscureText: hidePassword,
+                  showCursor: false,
+                  decoration: InputDecoration(
+                      isDense: true,
+                      prefixIcon: const Icon(Icons.lock),
+                      hintText: "Entre Mot de passe",
+                      hintStyle: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .copyWith(
+                              color: Colors.black38,
+                              fontWeight: FontWeight.w500),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.black12, width: 1),
+                          borderRadius: BorderRadius.circular(12)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.black38, width: 1),
+                          borderRadius: BorderRadius.circular(12)),
+                      counterText: ""),
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w500,
+                      ),
+                  onChanged: (String value) {},
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Veuiller entrer un mot de passe";
+                    }
+                    return null;
+                  },
+                );
+              }))),
           Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               child: MaterialButton(
@@ -82,7 +114,7 @@ class LoginPageState extends State<ParentLoginPage> {
                   "connexion",
                   style: TextStyle(fontSize: 23),
                 ),
-              ))
+              )),
         ],
       ),
     );
